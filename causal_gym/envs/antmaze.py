0a5e08874752b414de9377cd8463a957566cf72f
EylamTagor <eylamtagor@gmail.com>
2025-11-07 11:23:07 -0500

    antmaze expert from dataset

--- causal_gym/envs/antmaze.py
+++ causal_gym/envs/antmaze.py
@@ -8,7 +8,7 @@ from causal_gym.core import ActType, Graph
 from gymnasium import spaces
 
 class AntMazeSCM(SCM):
-    def __init__(self, env_id: str = 'antmaze-medium-navigate-v0', num_steps: int = 1000, hidden_dims: Optional[Set[str]] = None, seed: Optional[int] = None):
+    def __init__(self, env_id: str = 'antmaze-medium-navigate-singletask-task1-v0', num_steps: int = 1000, hidden_dims: Optional[Set[str]] = None, seed: Optional[int] = None):
         super().__init__()
 
         self.rng = np.random.default_rng(seed)
@@ -16,7 +16,7 @@ class AntMazeSCM(SCM):
         self.hidden_dims = hidden_dims if hidden_dims is not None else set()
         self._t = 0
 
-        self._env = ogbench.make_env_and_datasets(env_id, env_only=True, max_episode_steps = num_steps)
+        self._env = ogbench.make_env_and_datasets(env_id, env_only=True, max_episode_steps=num_steps)
         self._env.reset(seed=seed)
 
         self.P = [] # position, 3-dimensional vector of x,y,z
@@ -29,7 +29,7 @@ class AntMazeSCM(SCM):
         self._Y = [] # sparse reward
 
         self.action_space = self._env.action_space # Box(-1.0, 1.0, (8,), float32)
-        
+
         # build appropriate observation space
         act_low  = np.asarray(self.action_space.low, dtype=self.action_space.dtype)
         act_high = np.asarray(self.action_space.high, dtype=self.action_space.dtype)
@@ -273,7 +273,7 @@ class AntMazeSCM(SCM):
             p, o, a, l, t, j, x = base, base + 1, base + 2, base + 3, base + 4, base + 5, base + 6
             p2, o2, a2, l2, t2, j2, x2 = base_next, base_next + 1, base_next + 2, base_next + 3, base_next + 4, base_next + 5, base_next + 6
 
-            base_graph[x][j2] = 1 # torque impacts joint angles
+            base_graph[x][j2] = 1 # torque impacts joint angular velocity
 
             # state persistence
             base_graph[p][p2] = 1
@@ -294,7 +294,6 @@ class AntMazeSCM(SCM):
         graph = Graph(nodes=nodes, edges=edges)
         return graph
 
-
     @property
     def observed_unobserved_vars(self) -> Tuple[list[str], list[str]]:
         all_vars = ['P', 'O', 'A', 'L', 'T', 'J', 'X']
@@ -302,13 +301,110 @@ class AntMazeSCM(SCM):
         unobserved = list(self.hidden_dims) + ['Y']
         return observed, unobserved
 
+class AntMazeExpert:
+    def __init__(self, env_id: str = 'antmaze-medium-navigate-singletask-task1-v0', num_steps: int = 1000, hidden_dims: Optional[Set[str]] = None, seed: Optional[int] = None):
+        self.rng = np.random.default_rng(seed)
+        self.num_steps = num_steps
+
+        _, train, test = ogbench.make_env_and_datasets(env_id, max_episode_steps=num_steps, compact_dataset=True)
+        self._expert_trajs = {k: np.concatenate([train[k], test[k]], axis=0) for k in train.keys()}
+
+        self.num_eps = len(self._expert_trajs['observations']) // 1000
+        self._ep = -1
+        self._t = 0
+
+        self.hidden_dims = hidden_dims if hidden_dims is not None else set()
+
+        self.P = []
+        self.O = []
+        self.A = []
+        self.L = []
+        self.T = []
+        self.J = []
+        self.X = []
+        self._Y = []
+
+    def observation(self) -> Dict[str, Any]:
+        obs = {}
+
+        if 'P' not in self.hidden_dims:
+            obs['P'] = self.P
+        if 'O' not in self.hidden_dims:
+            obs['O'] = self.O
+        if 'A' not in self.hidden_dims:
+            obs['A'] = self.A
+        if 'L' not in self.hidden_dims:
+            obs['L'] = self.L
+        if 'T' not in self.hidden_dims:
+            obs['T'] = self.T
+        if 'J' not in self.hidden_dims:
+            obs['J'] = self.J
+        if 'X' not in self.hidden_dims:
+            obs['X'] = self.X
+
+        return obs
+
+    def reset(self):
+        self._ep += 1
+        self._t = self._ep * self.num_steps
+        if self._t >= len(self._expert_trajs['observations']):
+            raise ValueError("No more expert trajectories available.")
+
+        self.P = []
+        self.O = []
+        self.A = []
+        self.L = []
+        self.T = []
+        self.J = []
+        self.X = []
+        self._Y = []
+
+    def step(self):
+        obs = self._expert_trajs['observations'][self._t]
+        action = self._expert_trajs['actions'][self._t].astype(np.float64)
+
+        reward = self._expert_trajs['rewards'][self._t]
+        terminated = self._expert_trajs['terminals'][self._t]
+        truncated = False # never happens in an OGBench dataset
+
+        self.P.append(obs[0:3])
+        self.O.append(obs[3:7])
+        self.A.append(obs[7:15])
+        self.L.append(obs[15:18])
+        self.T.append(obs[18:21])
+        self.J.append(obs[21:29])
+        self.X.append(action)
+        self._Y.append(reward)
+
+        hiddens = {}
+        if 'P' in self.hidden_dims:
+            hiddens['P'] = self.P
+        if 'O' in self.hidden_dims:
+            hiddens['O'] = self.O
+        if 'A' in self.hidden_dims:
+            hiddens['A'] = self.A
+        if 'L' in self.hidden_dims:
+            hiddens['L'] = self.L
+        if 'T' in self.hidden_dims:
+            hiddens['T'] = self.T
+        if 'J' in self.hidden_dims:
+            hiddens['J'] = self.J
+        if 'X' in self.hidden_dims:
+            hiddens['X'] = self.X
+
+        info = {'Y': self._Y, 'env_obs': obs, 'env_info': {}, 'hidden_obs': hiddens, 'natural_action': action}
+
+        self._t += 1
+        return self.observation(), reward, terminated, truncated, info
+
 class AntMazePCH(PCH):
-    def __init__(self, env_id: str = 'antmaze-large-navigate-v0', num_steps: int = 1000, hidden_dims: Optional[Set[str]] = None, seed: Optional[int] = None):
+    def __init__(self, env_id: str = 'antmaze-medium-navigate-singletask-task1-v0', num_steps: int = 1000, hidden_dims: Optional[Set[str]] = None, seed: Optional[int] = None):
         # initialize underlying SCM
         self.env = AntMazeSCM(env_id=env_id, num_steps=num_steps, hidden_dims=hidden_dims, seed=seed)
+        self.expert = AntMazeExpert(env_id=env_id, num_steps=num_steps, hidden_dims=hidden_dims, seed=seed)
         super().__init__()
 
-    def see(self, behavioral_policy=None, show_reward = True) -> Tuple[Any, Any, float, bool, bool, Dict[str, Any]]:
+    def see(self, behavioral_policy=None, show_reward=True) -> Tuple[Any, Any, float, bool, bool, Dict[str, Any]]:
         P = self.env.P
         O = self.env.O
         A = self.env.A
@@ -319,7 +415,7 @@ class AntMazePCH(PCH):
         if behavioral_policy is not None:
             action = behavioral_policy(P, O, A, L, T, J)
         else:
-            action = self.env.action(P, O, A, L, T, J)
+            return self.expert.step()
 
         obs, reward, terminated, truncated, info = self.env.step(action, history=True, show_reward=show_reward)
         info['natural_action'] = action
@@ -327,11 +423,11 @@ class AntMazePCH(PCH):
 
     # Interventional step with forced action
     def do(self, do_policy, show_reward = True):
-        action = do_policy(self.env.observation())
+        action = do_policy(self.env.observation(history=True))
         o, r, term, trunc, info = self.env.step(action, history=True, show_reward=show_reward)
         info['action'] = action
         return o, r, term, trunc, info
-    
+
     # Counterfactual policy intervention
     def ctf_do(self, ctf_policy):
         P = self.env.P
@@ -347,16 +443,17 @@ class AntMazePCH(PCH):
         info['natural_action'] = intuition
         info['action'] = action
         return obs, r, terminated, truncated, info
-    
+
     def reset(self, *, seed: int = None) -> Tuple[Any, dict]:
+        self.expert.reset()
         return self.env.reset(history=True, seed=seed)
 
     def render(self) -> Any:
         return self.env.render()
-    
+
     def close(self) -> None:
         self.env.close()
-    
+
     @property
     def get_graph(self) -> Graph:
         return self.env.get_graph

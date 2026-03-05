e5517428c40eaf326b0537e6eab78923ad54513c
Eylam Tagor <eylamtagor@gmail.com>
2026-03-05 07:40:45 -0500

    cube env from ogbench

--- causal_gym/envs/__init__.py
+++ causal_gym/envs/__init__.py
@@ -14,6 +14,7 @@ from .highway_single_step import HighwaySingleStepSCM, HighwaySingleStepPCH
 from .highway import HighwaySCM, HighwayPCH
 from .antmaze import AntMazeSCM, AntMazePCH
 from .humanoidmaze import HumanoidMazeSCM, HumanoidMazePCH
+from .cube import CubeSCM, CubePCH
 from .humanoidmaze_v2 import HumanoidMazeV2SCM, HumanoidMazeV2PCH
 from .pointmaze import PointMazeSCM, PointMazePCH
 from .race import RaceSCM, RacePCH
